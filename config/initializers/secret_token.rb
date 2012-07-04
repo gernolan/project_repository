7d71b6045d58a6da0fdc863074ae471c89c3dde4
Gerard Nolan <ger_sharon@hotmail.com>
2012-07-04 21:26:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WoodwindShop::Application.config.secret_token = 'f26185feb503482a2a5ab20e3147aba760af6d31f62fdb0b0fb5fb06ba5d6223d846bae9e2a894f9fb549e248ac85ebfe68542ccff5a5127abb1cd8919394fc5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WoodwindShop::Application.config.session_store :cookie_store, :key => '_woodwind_shop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WoodwindShop::Application.config.session_store :active_record_store
